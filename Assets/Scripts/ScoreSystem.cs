5088d7cbeeb6cff7d249674b64016e4f164ccdff
AsaTi <pommermann@hotmail.de>
2020-03-27 20:30:19 +0100

    Added Simple ScoreSystem + MagmaWall

---
+++ Assets/Scripts/ScoreSystem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreSystem : MonoBehaviour
+{
+    private float multiplier = 5f;
+    private float score = 0;
+    [SerializeField]
+    private TextMeshProUGUI text;
+
+    private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.activeSelf)
+        {
+
+            score += Time.deltaTime * multiplier;
+        }
+        text.text = "Score: " + Mathf.Floor(score);
+    }
+}
