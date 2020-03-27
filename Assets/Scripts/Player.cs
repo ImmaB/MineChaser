42f898d4c0b3ee6eb80672ff0c5226996b39952b
Imma <imma.becker@outlook.de>
2020-03-27 15:00:15 +0100

    trying new input system

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // if (AttitudeSensor.current == null)
+        //     Debug.LogError("No AttitudeSensor found");
+        // else
+        //     InputSystem.EnableDevice(AttitudeSensor.current);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnRotate(InputAction.CallbackContext ctx)
+    {
+        // Quaternion rot = ctx.ReadValue<Quaternion>();
+        // Debug.Log(rot);
+    }
+}
