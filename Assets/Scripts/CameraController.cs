cec28187660a7f076b93486d9f0828d9154d6533
Imma <imma.becker@outlook.de>
2020-03-28 20:37:32 +0100

    exchanged wheeljoint by hingejoint

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,8 +6,15 @@ public class CameraController : MonoBehaviour
 {
     [SerializeField] private GameObject followTarget;
 
+
+    private void Awake()
+    {
+        if (!followTarget)
+            followTarget = GameObject.FindGameObjectWithTag("Player");         
+    }
+
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         if (followTarget)
             transform.position = followTarget.transform.position.WithZ(transform.position.z);
