8c1b63e84a190e8514b21388d0e4f42561c27b84
AsaTi <pommermann@hotmail.de>
2020-03-29 16:48:57 +0200

    Update Player.Pref + Level01 testing

---
+++ Assets/Scripts/PlayerHead.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHead : MonoBehaviour
+{
+    public int bumpCount;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        bumpCount++;
+        Debug.Log(bumpCount);
+    }
+}
