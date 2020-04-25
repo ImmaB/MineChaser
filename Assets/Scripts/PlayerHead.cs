edb839bdc8fcdd7a4a3f681bf3f1e074986754c3
AsaTi <pommermann@hotmail.de>
2020-04-25 18:19:00 +0200

    Working On Level01_Loop

--- Assets/Scripts/PlayerHead.cs
+++ Assets/Scripts/PlayerHead.cs
@@ -15,8 +15,40 @@ public class PlayerHead : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        bumpCount++;
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            bumpCount = 3;
+        }
+        else
+        {
+            bumpCount++;
+        }
+        //Better use if else
+        switch (bumpCount)
+        {
+            case 1:
+                //graphic change crack 1
+                break;
+            case 2:
+                //Graphic no Helmet
+                break;
+            case 3:
+                Dead();
+                break;
+            default:
+                break;
+        }
         SoundManager.PlayHeadHit();
         Debug.Log(bumpCount);
     }
+    private void Dead()
+    {
+        // Not Tested
+        FindObjectOfType<ResultScreen>().gameObject.SetActive(true);
+        //Animation?/Graphic change?
+        gameObject.SetActive(false);
+        //Stop Camera Movement
+        Debug.LogError("DEADSCREEN NEEDED");
+    }
 }
+
