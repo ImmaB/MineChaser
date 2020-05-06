e2b26198ae87045e827d1bdef5cb4902d9c3e90c
AsaTi <pommermann@hotmail.de>
2020-05-06 15:16:09 +0200

    Loop_01 mechanics

---
+++ Assets/Scripts/CollectibleRespawn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectibleRespawn : MonoBehaviour
+{
+    [SerializeField]
+    GameObject collectibleParent;
+
+    [SerializeField]
+    GameObject blockade;
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        for (int i = 0; i < collectibleParent.transform.childCount; i++)
+        {
+            collectibleParent.transform.GetChild(i).gameObject.SetActive(true);
+        }
+        blockade.SetActive(false);
+    }
+}
