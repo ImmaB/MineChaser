6d243a10668f73c4779182ec743e7071fb279c35
AsaTi <pommermann@hotmail.de>
2020-06-06 18:34:19 +0200

    Added InfoPanel to TitleScene

---
+++ Assets/Scripts/TitlescreenInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitlescreenInfo : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject infoScreen;
+
+    public void ActivateScreen()
+    {
+        infoScreen.SetActive(true);
+    }
+    public void DeactivateScreen()
+    {
+        infoScreen.SetActive(false);
+    }
+}
